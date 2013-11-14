a19d58be9fd5215c489243471dde6b86e0f87234
Rafael Lugli <rafaellugli@hotmail.com>
2013-11-14 20:57:45 -0200

    Criado classe ponto.

--- src/edu/ibta/cg2/model/Ponto.java
+++ src/edu/ibta/cg2/model/Ponto.java
@@ -1,5 +1,26 @@
 package edu.ibta.cg2.model;
 
 public class Ponto {
-
+	
+	private int x;
+	private int y;
+	
+	public Ponto(int x, int y){
+		setX(x);
+		setY(y);
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 }
