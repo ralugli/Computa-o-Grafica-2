badc2e1ea0c7cf0ae2f635fb69cfe20f128e140f
moleiroo <moleiroo@gmail.com>
2013-11-18 21:15:30 -0200

    adicionadas translacao, escalonamento e modificados os metodos do Poligono para retornar os poliginos modificados

--- src/edu/ibta/cg2/model/Poligono.java
+++ src/edu/ibta/cg2/model/Poligono.java
@@ -11,39 +11,75 @@ public class Poligono {
 		lista.add(ponto);
 	}
 
-	public void espelhamento_horizontal(ArrayList lista) {
+	public ArrayList espelhamento_horizontal(ArrayList lista) {
 
 		int tamanho = lista.size(); // pega o numero de pontos no poligono
 		int i;
 		Ponto atual; // ponto em que as modificações serão feitas
-		//Poligono novo = new Poligono(null); // este poligono será o resultado
-											// das operações
+		// Poligono novo = new Poligono(null); // este poligono será o resultado
+		// das operações
 		ArrayList<Ponto> nova_lista = new ArrayList<Ponto>();
 
-		for (i = 0; i <= tamanho; i++) {  //este for torna os valores
-			atual = (Ponto) lista.get(i); //originais do poligono negativos
-			atual.x = (atual.x - (atual.x * 2)); //e armazena-os em uma nova array
+		for (i = 0; i <= tamanho; i++) { // este for torna os valores
+			atual = (Ponto) lista.get(i); // originais do poligono negativos
+			atual.x = (atual.x - (atual.x * 2)); // e armazena-os em uma nova
+													// array
 			nova_lista.add(atual);
 
 		}
-
-		
+return nova_lista;
 	}
 
-	public void espelhamento_vertical(ArrayList lista) {
-		
-		int tamanho = lista.size(); 
+	public ArrayList espelhamento_vertical(ArrayList lista) {
+
+		int tamanho = lista.size();
 		int i;
 		Ponto atual;
 		ArrayList<Ponto> nova_lista = new ArrayList<Ponto>();
 
-		for (i = 0; i <= tamanho; i++) {  
-			atual = (Ponto) lista.get(i); 
-			atual.y = (atual.y - (atual.y * 2)); 
+		for (i = 0; i <= tamanho; i++) {
+			atual = (Ponto) lista.get(i);
+			atual.y = (atual.y - (atual.y * 2));
 			nova_lista.add(atual);
 
 		}
+return nova_lista;
+	}
 
+	public ArrayList translacao(ArrayList lista, int distancia) {
+
+		int tamanho = lista.size();
+		int i;
+		Ponto atual;
+		ArrayList<Ponto> nova_lista = new ArrayList<Ponto>();
+
+		for (i = 0; i < tamanho; i++) {
+			// calculo da translacao
+			atual = (Ponto) lista.get(i);
+			atual.y = (atual.y + distancia);
+			atual.x = (atual.x + distancia);
+			nova_lista.add(atual);
+		}
+return nova_lista;
+	}
+
+	public ArrayList
+	escalonamento(ArrayList lista, int dimensao) {
+		int tamanho = lista.size();
+		int i;
+		Ponto atual;
+		ArrayList<Ponto> nova_lista = new ArrayList<Ponto>();
+
+		for (i = 0; i < tamanho; i++) {
+
+			atual = (Ponto) lista.get(i);
+			atual.x = atual.x * dimensao;
+			atual.y = atual.y * dimensao;
+			nova_lista.add(atual);
+			
+
+		}
+		return nova_lista;
 	}
 
 }
